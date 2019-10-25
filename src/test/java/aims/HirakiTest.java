2c63d95e73dbc4813c578fdef8fb85f31e2f148a
JinOhKwon <kjo1333@gmail.com>
2019-10-25 14:10:15 +0900

    히라키 테스트 임시주석처리

--- src/test/java/aims/HirakiTest.java
+++ src/test/java/aims/HirakiTest.java
@@ -1,72 +1,72 @@
-package aims;
-
-
-//HirakiTest
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.sql.SQLException;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class HirakiTest {
-//    @Autowired
-//    UserAwareService userAwareService;
+//package aims;
 //
-//    @Autowired
-//    DataSource dataSource;
-
+//
+////HirakiTest
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.sql.SQLException;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class HirakiTest {
+////    @Autowired
+////    UserAwareService userAwareService;
+////
+////    @Autowired
+////    DataSource dataSource;
+//
+////    @Test
+////    public void exampleTest() {
+////        long startTime = System.currentTimeMillis();
+////
+////        for (int i = 0; i < 10000; i++) {
+////            List<User> users = userAwareService.findAll();
+////        }
+////
+////        long endTime = System.currentTimeMillis();
+////
+////        long lTime = endTime - startTime;
+////        System.out.println("TIME : " + lTime + "(ms)");
+////    }
+//
 //    @Test
-//    public void exampleTest() {
-//        long startTime = System.currentTimeMillis();
-//
-//        for (int i = 0; i < 10000; i++) {
-//            List<User> users = userAwareService.findAll();
-//        }
-//
-//        long endTime = System.currentTimeMillis();
-//
-//        long lTime = endTime - startTime;
-//        System.out.println("TIME : " + lTime + "(ms)");
+//    public void helloworld() throws SQLException, InterruptedException {
+////        Thread[] thread = new Thread[1000];
+////        Runnable[] run = new Runnable[1000];
+////
+////        for (int j = 0; j < 1000; j++) {
+////            run[j] = new Runnable() {
+////                @Override
+////                public void run() {
+////                    try {
+////                        for (int i = 0; i < 100; i++) {
+////                            Connection con = dataSource.getConnection();
+////                            PreparedStatement psmt = con.prepareStatement("SELECT * FROM TB_USER");
+////                            ResultSet rs = psmt.executeQuery();
+////                            //psmt.close();
+////                            con.close();
+////                        }
+////                    } catch (SQLException e) {
+////                        e.printStackTrace();
+////                    }
+////                }
+////            };
+////        }
+////
+////        long start = System.currentTimeMillis();
+////        for (int i = 0; i < 500; i++) {
+////            thread[i] = new Thread(run[i]);
+////            thread[i].start();
+////            thread[i].join();
+////            System.out.println(i);
+////        }
+////
+////        long end = System.currentTimeMillis();
+////        System.out.println(String.format("dataSource NAME::::: %s   ", dataSource));
+////        System.out.println(String.format("TIME %d", end - start));
 //    }
-
-    @Test
-    public void helloworld() throws SQLException, InterruptedException {
-//        Thread[] thread = new Thread[1000];
-//        Runnable[] run = new Runnable[1000];
-//
-//        for (int j = 0; j < 1000; j++) {
-//            run[j] = new Runnable() {
-//                @Override
-//                public void run() {
-//                    try {
-//                        for (int i = 0; i < 100; i++) {
-//                            Connection con = dataSource.getConnection();
-//                            PreparedStatement psmt = con.prepareStatement("SELECT * FROM TB_USER");
-//                            ResultSet rs = psmt.executeQuery();
-//                            //psmt.close();
-//                            con.close();
-//                        }
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            };
-//        }
-//
-//        long start = System.currentTimeMillis();
-//        for (int i = 0; i < 500; i++) {
-//            thread[i] = new Thread(run[i]);
-//            thread[i].start();
-//            thread[i].join();
-//            System.out.println(i);
-//        }
-//
-//        long end = System.currentTimeMillis();
-//        System.out.println(String.format("dataSource NAME::::: %s   ", dataSource));
-//        System.out.println(String.format("TIME %d", end - start));
-    }
-}
+//}
